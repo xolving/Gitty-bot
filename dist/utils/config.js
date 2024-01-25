83307838a28cf849638dca96c8e1cf6c70f574c3
enbraining <me@xolving.com>
2024-01-25 16:38:03 +0900

    🔥 dist가 포함되지 않게 변경됨

--- dist/utils/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-require("dotenv/config");
-exports.config = {
-    discordToken: (_a = process.env.DISCORD_TOKEN) !== null && _a !== void 0 ? _a : "",
-};
