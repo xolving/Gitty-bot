a1b2b609903fab2744a3807388aa74369ea1dca3
enbraining <me@xolving.com>
2024-02-25 00:08:21 +0900

    remove default github token

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,14 +2,12 @@ import "dotenv/config";
 
 export interface Config {
   discordToken: string;
-  githubToken: string;
   supabaseUrl: string;
   supabaseAnonKey: string;
 }
 
 export const config: Config = {
   discordToken: process.env.DISCORD_TOKEN ?? "",
-  githubToken: process.env.GITHUB_TOKEN ?? "",
   supabaseUrl: process.env.SUPABASE_URL ?? "",
   supabaseAnonKey: process.env.SUPABASE_ANON_KEY ?? ""
 };
